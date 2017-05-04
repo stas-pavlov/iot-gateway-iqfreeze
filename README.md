95567342220c3c015fb60a1188bdb23dce6ae8ac
Stas Pavlov <stanisp@microsoft.com>
2017-05-04 13:04:00 +0300

    Add Azure Function code and change readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Driving a refrigerated truck to Azure
 ## Making an IoT solution more SaaS-ready with Quarta and TechnoKom
 
-Placeholder for code examples to demonstrate usage of Azure Cloud Services, Azure Table, Azure Functions, Azure Stream Analytics for an IoT solution 
+Code examples to demonstrate usage of Azure Cloud Services, Azure Table, Azure Functions, Azure Stream Analytics for an IoT solution 
+
+You need to have Cloud Services, Azure Storage Account and IoT Hub to have the demo run.  
+Detailed instruction to follow.
---
+++ code/Function/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// This function is triggered each time a message is revieved in the IoTHub.
+// The message payload is persisted in an Azure Storage Table
+var moment = require('moment');
+
+module.exports = function (context, iotHubMessage) {
+    context.log('Message received: ' + JSON.stringify(iotHubMessage));
+    context.bindings.outputTable = {
+        "partitionKey": moment.utc().format('YYYYMMDD'),
+        "rowKey": moment.utc().format('hhmmss') + process.hrtime()[1] + '',
+        "message": JSON.stringify(iotHubMessage)
+    };
+    context.done();
+};
